357dd859cfac845d57de7f0ef30468fafcb274e6
Algense17 <leonv909@gmail.com>
2025-01-26 20:36:52 +0300

    stage4

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -7,24 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
     
-    @IBOutlet weak var redCircle: UIView!
-    @IBOutlet weak var yellowCircle: UIView!
-    @IBOutlet weak var greenCircle: UIView!
+    @IBOutlet weak var redCircleView: UIView!
+    @IBOutlet weak var yellowCircleView: UIView!
+    @IBOutlet weak var greenCircleView: UIView!
+    
     @IBOutlet weak var buttonSwichLabel: UIButton!
     
     var isFirstTab = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redCircle.layer.cornerRadius = 50
-        redCircle.alpha = 0.3
-        yellowCircle.layer.cornerRadius = 50
-        yellowCircle.alpha = 0.3
-        greenCircle.layer.cornerRadius = 50
-        greenCircle.alpha = 0.3
+        redCircleView.layer.cornerRadius = 50
+        redCircleView.alpha = 0.3
+        yellowCircleView.layer.cornerRadius = 50
+        yellowCircleView.alpha = 0.3
+        greenCircleView.layer.cornerRadius = 50
+        greenCircleView.alpha = 0.3
         
         buttonSwichLabel.layer.cornerRadius = 10
     }
@@ -33,18 +34,18 @@ class ViewController: UIViewController {
         
         if isFirstTab {
             buttonSwichLabel.setTitle("Next", for: .normal)
-            redCircle.alpha = 1
+            redCircleView.alpha = 1
             isFirstTab = false
         } else {
-            if redCircle.alpha == 1 {
-                redCircle.alpha = 0.3
-                yellowCircle.alpha = 1
-            } else if yellowCircle.alpha == 1 {
-                yellowCircle.alpha = 0.3
-                greenCircle.alpha = 1
-            } else if greenCircle.alpha == 1 {
-                redCircle.alpha = 1
-                greenCircle.alpha = 0.3
+            if redCircleView.alpha == 1 {
+                redCircleView.alpha = 0.3
+                yellowCircleView.alpha = 1
+            } else if yellowCircleView.alpha == 1 {
+                yellowCircleView.alpha = 0.3
+                greenCircleView.alpha = 1
+            } else if greenCircleView.alpha == 1 {
+                redCircleView.alpha = 1
+                greenCircleView.alpha = 0.3
             }
         }
     }
